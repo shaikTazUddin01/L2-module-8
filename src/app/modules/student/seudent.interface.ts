83da1da16b7d8787339200dae28138a97645fc90
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-05-17 01:17:29 +0600

    added 8.5

---
+++ src/app/modules/student/seudent.interface.ts
@@ -0,0 +1,30 @@
+import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+export type Guardian={
+    fatherName:string;
+    fatherOccupation:string;
+    fatherContactNo:string;
+    motherName:string;
+    motherOccupation:string;
+    motherContactNo:string;
+}
+
+
+export type Student = {
+  id: string;
+  name: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodgroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress:string;
+  permanentAddress:string;
+  guardian:Guardian;
+};
